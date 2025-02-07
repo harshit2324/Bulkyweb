9c5ca5ed4bf81286778dda112622949d3e77af14
Harshit Richhariya <richhariyaharshit@outlook.com>
2025-02-07 12:22:46 +0530

    create a db connection and add connect a database with entity framework

---
+++ Bulkyweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Bulkyweb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulkyweb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { id = 2, Name = "Action", DisplayOrder = 3 },
+                new Category { id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
+
--- Bulkyweb/Program.cs
+++ Bulkyweb/Program.cs
@@ -1,9 +1,14 @@
+using Bulkyweb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options=> 
+options.UseSqlServer(builder.Configuration.GetConnectionString("defualtconnection")));
 
-var app = builder.Build();
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
