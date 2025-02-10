7c568ee67e60788b7bf0db00e69247f5df0f2a48
Harshit Richhariya <richhariyaharshit684@gmail.com>
2025-02-10 22:41:11 +0530

    Razor temp project complete

---
+++ BulkyWebRazor_Temp/Data/ApplicatiodbContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationdbContext : DbContext
+    {
+        public ApplicationdbContext(DbContextOptions<ApplicationdbContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { id = 2, Name = "Action", DisplayOrder = 3 },
+                new Category { id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
--- BulkyWebRazor_Temp/Program.cs
+++ BulkyWebRazor_Temp/Program.cs
@@ -1,7 +1,13 @@
+using BulkyWebRazor_Temp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext <ApplicationdbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("defualtconnection")));
+
 
 var app = builder.Build();
 
