33710532135a17583bdffcfc07cf22a89a19fca1
Harshit Richhariya <richhariyaharshit684@gmail.com>
2025-02-10 08:48:24 +0530

    add: new razor project configration

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [Range(1, 100)]
+        public int DisplayOrder { get; set; }
+    }
+}
