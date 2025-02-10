18ce84069e5a43b0a98ea819f4c2993d404f7d65
Harshit Richhariya <richhariyaharshit@outlook.com>
2025-02-10 08:11:46 +0530

    project complete [

--- Bulkyweb/Controllers/CategoryController.cs
+++ Bulkyweb/Controllers/CategoryController.cs
@@ -17,5 +17,81 @@ namespace Bulkyweb.Controllers
           List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "the displayorder cannot exactly match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+           
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryfromdb = _db.Categories.Find(id);
+            if(categoryfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromdb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+         
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryfromdb = _db.Categories.Find(id);
+            if (categoryfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromdb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+           
+        }
     }
 }
--- Bulkyweb/Models/Category.cs
+++ Bulkyweb/Models/Category.cs
@@ -7,7 +7,9 @@ namespace Bulkyweb.Models
         [Key]
        public int id { get; set; }
         [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
+        [Range(1,100)]
          public int DisplayOrder {  get; set; }
     }
 }
